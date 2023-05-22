82376754e3b7ad739f4ff2743f94f5460b4a03a9
kamikaonashi <thorschi93@gmx.at>
2023-05-22 06:40:54 +0200

    add kprofiles

--- common.mk
+++ common.mk
@@ -298,6 +298,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
