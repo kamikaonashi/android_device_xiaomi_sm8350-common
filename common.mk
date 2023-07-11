6c36f6b33233a361426c34259df52965c051e220
kamikaonashi <thorschi93@gmx.at>
2023-07-11 10:32:13 +0200

    sm8350-common: drop livedisplay

--- common.mk
+++ common.mk
@@ -316,8 +316,8 @@ PRODUCT_PACKAGES += \
     KProfiles
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.1-service.xiaomi_sm8350
 
 # Media
 PRODUCT_PACKAGES += \
