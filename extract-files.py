3d0535a0616f01aec3eafd0d7f0b087a0873b3a1
basamaryan <basam.aryan@gmail.com>
2025-07-08 01:26:51 +0800

    sm8350-common: Shim libwfdservice with libaudioclient_shim
    Change-Id: Idd6603ad16926226f22423a2bba2075d430015d5

--- extract-files.py
+++ extract-files.py
@@ -53,7 +53,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
-    'system_ext/lib64/libwfdservice.so': blob_fixup()
+    ('system_ext/lib/libwfdservice.so', 'system_ext/lib64/libwfdservice.so'): blob_fixup()
+        .add_needed('libaudioclient_shim.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so')
